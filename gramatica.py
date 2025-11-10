11ad63fbf8343e929691991c61fb3a1eb81905f5
Emanuel Hrițuleac <hrituleacemanuel@gmail.com>
2025-11-10 10:10:48 +0200

    add function to verify a string if is part of the language

--- gramatica.py
+++ gramatica.py
@@ -1,11 +1,29 @@
 from typing import List
 
 class Gramatica:
-    def __init__(self, listaNeterminale, listaTerminale, simbolStart, listaProductii):
-        self.listaNeterminale = listaNeterminale
-        self.listaTerminale = listaTerminale
-        self.simbolStart = simbolStart
-        self.listaProductii = listaProductii
+    def __init__(self, numeFisier: str, numeFisierTabel: str = None):
+        self.listaNeterminale = []
+        self.listaTerminale = []
+        self.simbolStart = ""
+        self.listaProductii = []
+        self.tabel = None
+        self.citesteGramaticaDinFisier(numeFisier)
+        
+        # Citește tabelul dacă e furnizat
+        if numeFisierTabel:
+            self.tabel = TabelGramatica(numeFisierTabel)
+        
+    def citesteGramaticaDinFisier(self, numeFisier: str):
+        with open(numeFisier, 'r') as f:
+            linii = f.readlines()
+            self.listaNeterminale = linii[0].strip().split(" ")
+            self.listaTerminale = linii[1].strip().split(" ")
+            self.simbolStart = linii[2].strip()
+            self.listaProductii = []
+            for i in range(3, len(linii)):
+                linie = linii[i].split("->")
+                productie = Productie(linie[0].strip(), linie[1].strip())
+                self.adaugaProductie(productie)
 
     def adaugaProductie(self, productie):
             self.listaProductii.append(productie)
@@ -44,8 +62,137 @@ class Gramatica:
                                 noulSir = sirCurent[:i] + productie.sirInlocuire + sirCurent[i+1:]
                                 listaSiruri.append(noulSir)
                         break
+    
+    def verificaSir(self, sir: str, afiseazaPasi: bool = True):
+        """
+        Verifică dacă un șir aparține gramaticii folosind algoritmul LR (shift-reduce).
+        Folosește tabelul de acțiuni și tabelul de salt.
         
-
+        Args:
+            sir: Șirul de verificat
+            afiseazaPasi: Dacă True, afișează pașii algoritmului
+            
+        Returns:
+            True dacă șirul este acceptat, False altfel
+        """
+        if not self.tabel:
+            print("Eroare: Tabelul de analiză nu este încărcat!")
+            return False
+        
+        # Inițializare
+        stiva = [0]  # Stiva începe cu starea 0
+        sir_input = sir + '$'  # Adaugă marcatorul de final
+        pozitie = 0  # Poziția curentă în șir
+        
+        if afiseazaPasi:
+            print(f"\n{'Pas':<5} {'Stiva':<20} {'Input':<20} {'Actiune':<20}")
+            print("-" * 70)
+        
+        pas = 0
+        
+        while True:
+            pas += 1
+            
+            # Starea curentă = vârful stivei
+            stare_curenta = stiva[-1]
+            
+            # Simbolul curent din input
+            simbol_curent = sir_input[pozitie] if pozitie < len(sir_input) else '$'
+            
+            # Obține acțiunea din tabel
+            actiune = self.tabel.obtine(stare_curenta, simbol_curent)
+            
+            if afiseazaPasi:
+                stiva_str = ' '.join(str(x) for x in stiva)
+                input_str = sir_input[pozitie:]
+                print(f"{pas:<5} {stiva_str:<20} {input_str:<20} {actiune:<20}")
+            
+            # Verifică tipul acțiunii
+            if actiune == '0' or actiune == '':
+                # Eroare - acțiune invalidă
+                if afiseazaPasi:
+                    print(f"\nEroare: Nu există acțiune pentru starea {stare_curenta} și simbolul '{simbol_curent}'")
+                return False
+            
+            elif actiune == 'acc':
+                # Acceptare - șirul este valid
+                if afiseazaPasi:
+                    print("\nȘirul este ACCEPTAT!")
+                return True
+            
+            elif actiune[0] == 'd':
+                # Shift (deplasare) - d<stare>
+                try:
+                    stare_noua = int(actiune[1:])
+                    stiva.append(simbol_curent)  # Adaugă simbolul
+                    stiva.append(stare_noua)      # Adaugă starea nouă
+                    pozitie += 1                  # Avansează în input
+                except ValueError:
+                    if afiseazaPasi:
+                        print(f"\nEroare: Acțiune invalidă '{actiune}'")
+                    return False
+            
+            elif actiune[0] == 'r':
+                # Reduce (reducere) - r<numar_productie>
+                try:
+                    numar_productie = int(actiune[1:])
+                    
+                    # Tabelul indexează producțiile de la 1, dar lista noastră de la 0
+                    index_productie = numar_productie - 1
+                    
+                    # Găsește producția corespunzătoare
+                    if index_productie < 0 or index_productie >= len(self.listaProductii):
+                        if afiseazaPasi:
+                            print(f"\nEroare: Producția {numar_productie} nu există (index {index_productie})")
+                        return False
+                    
+                    productie = self.listaProductii[index_productie]
+                    
+                    # Numărul de simboluri din partea dreaptă a producției
+                    lungime_dreapta = len(productie.sirInlocuire)
+                    
+                    # Scoate 2 * lungime_dreapta elemente din stivă (stare + simbol pentru fiecare)
+                    # Dacă producția este epsilon (lungime 0), nu scoatem nimic
+                    if lungime_dreapta > 0:
+                        for _ in range(2 * lungime_dreapta):
+                            if len(stiva) > 1:  # Nu scoatem starea inițială 0
+                                stiva.pop()
+                    
+                    # Starea de pe vârful stivei după reducere
+                    stare_dupa_reduce = stiva[-1]
+                    
+                    # Neterminalul din partea stângă a producției
+                    neterminal = productie.simbolNeterminal
+                    
+                    # Obține starea de salt din tabel
+                    stare_salt = self.tabel.obtine(stare_dupa_reduce, neterminal)
+                    
+                    if stare_salt == '0' or stare_salt == '':
+                        if afiseazaPasi:
+                            print(f"\nEroare: Nu există salt pentru starea {stare_dupa_reduce} și neterminalul '{neterminal}'")
+                        return False
+                    
+                    # Adaugă neterminalul și starea de salt pe stivă
+                    stiva.append(neterminal)
+                    stiva.append(int(stare_salt))
+                    
+                except (ValueError, IndexError) as e:
+                    if afiseazaPasi:
+                        print(f"\nEroare la reducere: {e}")
+                    return False
+            
+            else:
+                # Acțiune necunoscută
+                if afiseazaPasi:
+                    print(f"\nEroare: Acțiune necunoscută '{actiune}'")
+                return False
+            
+            # Protecție împotriva buclelor infinite
+            if pas > 1000:
+                if afiseazaPasi:
+                    print("\nEroare: Prea mulți pași, posibilă buclă infinită")
+                return False
+        
 class Productie:
     def __init__ (self, simbolNeterminal, sirInlocuire):
         self.simbolNeterminal = simbolNeterminal
@@ -53,6 +200,48 @@ class Productie:
 
     def afiseazaProductie(self):
         print(self.simbolNeterminal + " -> " + self.sirInlocuire)
-
+        
+        
+class TabelGramatica:
+    def __init__(self, numeFisier: str):
+        self.tabel = {}
+        self.selectoriColoana = []
+        self.citesteTabelDinFisier(numeFisier)
+        
+    def citesteTabelDinFisier(self, numeFisier: str):
+        with open(numeFisier, 'r') as f:
+            linii = f.readlines()
+            
+            # Prima linie - selectori pentru coloane
+            self.selectoriColoana = linii[0].strip().split()
+            
+            # Restul liniilor - date (linia 1, 2, 3, ...)
+            for numarLinie in range(1, len(linii)):
+                valori = linii[numarLinie].strip().split()
+                if valori:  # Verifică că linia nu e goală
+                    # Creează dicționar pentru această linie (indexul liniei = numarLinie - 1)
+                    self.tabel[numarLinie - 1] = {}
+                    for i, selector in enumerate(self.selectoriColoana):
+                        self.tabel[numarLinie - 1][selector] = valori[i]
+    
+    def obtine(self, selectorLinie: int, selectorColoana: str):
+        """Returnează valoarea din tabel[selectorLinie][selectorColoana]"""
+        return self.tabel.get(selectorLinie, {}).get(selectorColoana, '0')
+    
+    def afiseazaTabel(self):
+        """Afișează tabelul într-un format frumos"""
+        # Header
+        print("     ", end="")
+        for selector in self.selectoriColoana:
+            print(f"{selector:5}", end="")
+        print()
+        
+        # Date
+        for linie in sorted(self.tabel.keys()):
+            print(f"{linie:4} ", end="")
+            for selector in self.selectoriColoana:
+                valoare = self.obtine(linie, selector)
+                print(f"{valoare:5}", end="")
+            print()
 
 lungimeMaximaSir = 10
--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-from gramatica import Gramatica, Productie
+from gramatica import Gramatica
 from gramatica import lungimeMaximaSir
 
-gramaticaText = open("gramatica.txt", "r")
-linii = gramaticaText.readlines()
+# Creează gramatica din fișier cu tabel
+gramatica = Gramatica("gramatica.txt", "tabel_gramatica.txt")
 
-listaNeterminale = linii[0].strip().split(" ")
-listaTerminale = linii[1].strip().split(" ")
-simbolStart = linii[2].strip()
-
-gramatica = Gramatica(listaNeterminale, listaTerminale, simbolStart, [])
-
-for i in range(3, len(linii)):
-    linie = linii[i].split("->")
-    productie = Productie(linie[0].strip(), linie[1].strip())
-    gramatica.adaugaProductie(productie)
-
+# Afișează gramatica
 gramatica.afiseazaGramatica()
 
-gramatica.genereazaLanturi(lungimeMaximaSir)
+# Afișează tabelul
+if gramatica.tabel:
+    print("\nTabel de analiza:")
+    gramatica.tabel.afiseazaTabel()
 
-gramaticaText.close()
+# Testează verificarea de șiruri
+print("\n" + "="*70)
+print("VERIFICARE SIRURI CU ALGORITMUL LR")
+print("="*70)
+
+# Șiruri de test
+siruri_test = [
+    "a+a",
+    "a*a",
+    "a+a*a",
+    "(a+a)",
+    "a+a+a",
+    "abc",  # Invalid
+    "(a",   # Invalid
+]
+
+for sir in siruri_test:
+    print(f"\n{'='*70}")
+    print(f"Verificare șir: '{sir}'")
+    print(f"{'='*70}")
+    rezultat = gramatica.verificaSir(sir, afiseazaPasi=True)
+    print(f"\nRezultat: {'✓ ACCEPTAT' if rezultat else '✗ RESPINS'}")
+
+# # Generează lanțuri (opțional - comentat pentru a nu încărca output-ul)
+# print("\n\nLanturi generate:")
+# gramatica.genereazaLanturi(lungimeMaximaSir)
 
